b2bb0223c7c404fd96d62a8843772b3a433a1ecc
bokutotu <mushin.hudoushin@gmail.com>
2024-06-22 03:40:06 +0900

    hoge

--- graph_api_conv_2.c
+++ graph_api_conv_2.c
@@ -1,73 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <cuda_runtime.h>
 #include <cudnn.h>
 #include <cudnn_graph.h>
 
-#define CHECK_CUDA(func)                                    \
-{                                                           \
-  cudaError_t status = (func);                              \
-  if (status != cudaSuccess) {                              \
-    fprintf(stderr, "CUDA Error: %s:%d, ", __FILE__, __LINE__); \
-    fprintf(stderr, "code: %d, reason: %s\n", status, cudaGetErrorString(status)); \
-    exit(-1);                                               \
-  }                                                         \
-}
-
-#define CHECK_CUDNN(func)                                   \
-{                                                           \
-  cudnnStatus_t status = (func);                            \
-  if (status != CUDNN_STATUS_SUCCESS) {                     \
-    fprintf(stderr, "CUDNN Error: %s:%d, ", __FILE__, __LINE__);  \
-    fprintf(stderr, "reason: %s\n", cudnnGetErrorString(status)); \
-    exit(-1);                                               \
-  }                                                         \
-}
-
-void assertDescriptorIsNull(cudnnBackendDescriptor_t desc) {
-  if (desc == NULL) {
-    fprintf(stderr, "Error: descriptor is not NULL\n");
-    exit(-1);
-  }
-}
-
-cudnnBackendDescriptor_t tensorDescriptorCreate(
-  int64_t numDim, 
-  int64_t *dim, 
-  int64_t *stride, 
-  int64_t byteAlignment, 
-  cudnnDataType_t dataType, 
-  int64_t name
-) {
-  cudnnBackendDescriptor_t tensorDesc;
-  CHECK_CUDNN(cudnnBackendCreateDescriptor(CUDNN_BACKEND_TENSOR_DESCRIPTOR, &tensorDesc));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
-                           CUDNN_ATTR_TENSOR_DATA_TYPE, CUDNN_TYPE_DATA_TYPE, 1, &dataType));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
-                          CUDNN_ATTR_TENSOR_DIMENSIONS, CUDNN_TYPE_INT64, numDim, dim));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
-                           CUDNN_ATTR_TENSOR_STRIDES, CUDNN_TYPE_INT64, numDim, stride));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
-                           CUDNN_ATTR_TENSOR_BYTE_ALIGNMENT, CUDNN_TYPE_INT64, 1, &byteAlignment));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
-                           CUDNN_ATTR_TENSOR_UNIQUE_ID, CUDNN_TYPE_INT64, 1, &name));
-  assertDescriptorIsNull(tensorDesc);
-
-  CHECK_CUDNN(cudnnBackendFinalize(tensorDesc));
-  assertDescriptorIsNull(tensorDesc);
-
-  return tensorDesc;
-}
+#include "utils.h"
 
 cudnnBackendDescriptor_t convolutionDescriptorCreate(
   int64_t numDim, 
---
+++ utils.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cudnn.h>
+#include <cudnn_graph.h>
+
+#include "utils.h"
+
+void assertDescriptorIsNull(cudnnBackendDescriptor_t desc) {
+  if (desc == NULL) {
+    fprintf(stderr, "Error: descriptor is not NULL\n");
+    exit(-1);
+  }
+}
+
+cudnnBackendDescriptor_t tensorDescriptorCreate(
+  int64_t numDim, 
+  int64_t *dim, 
+  int64_t *stride, 
+  int64_t byteAlignment, 
+  cudnnDataType_t dataType, 
+  int64_t name
+) {
+  cudnnBackendDescriptor_t tensorDesc;
+  CHECK_CUDNN(cudnnBackendCreateDescriptor(CUDNN_BACKEND_TENSOR_DESCRIPTOR, &tensorDesc));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
+                           CUDNN_ATTR_TENSOR_DATA_TYPE, CUDNN_TYPE_DATA_TYPE, 1, &dataType));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
+                          CUDNN_ATTR_TENSOR_DIMENSIONS, CUDNN_TYPE_INT64, numDim, dim));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
+                           CUDNN_ATTR_TENSOR_STRIDES, CUDNN_TYPE_INT64, numDim, stride));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
+                           CUDNN_ATTR_TENSOR_BYTE_ALIGNMENT, CUDNN_TYPE_INT64, 1, &byteAlignment));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendSetAttribute(tensorDesc, 
+                           CUDNN_ATTR_TENSOR_UNIQUE_ID, CUDNN_TYPE_INT64, 1, &name));
+  assertDescriptorIsNull(tensorDesc);
+
+  CHECK_CUDNN(cudnnBackendFinalize(tensorDesc));
+  assertDescriptorIsNull(tensorDesc);
+
+  return tensorDesc;
+}
